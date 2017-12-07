b7be57b57fe2c053768f92e57936a29ae7a226d3
Haley Lock <haleylock@Haleys-Air.fios-router.home>
2017-12-07 17:59:37 -0500

    first

---
+++ server.js
@@ -0,0 +1,24 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var debug = require('debug')('method-override')
+
+var app = express();
+var port = 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "wishes_db"
+});
