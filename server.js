de51c93333efab34399fbff92beb6615af554d00
Haley Lock <haleylock@Haleys-MacBook-Air.local>
2017-12-07 19:29:49 -0500

    make it better

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
-var express = require("express");
-var bodyParser = require("body-parser");
-var debug = require('debug')('method-override')
+//pull in required dependices 
+var express = require('express');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
 
 var app = express();
 var port = 3000;
+
+//to connect css to the page
+// Use the express.static middleware to serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
 
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -11,6 +16,7 @@ app.use(bodyParser.json());
 
 var exphbs = require("express-handlebars");
 
+//view engine is used for modernization and to create dynamic html
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
@@ -20,5 +26,5 @@ var connection = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "",
-  database: "wishes_db"
+  database: "burgers_db"
 });
